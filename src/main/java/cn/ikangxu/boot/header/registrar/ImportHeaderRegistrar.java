e81afe5f815d7c63a064028a76ae77fe64d7dc0e
IKangXu <1454900516@qq.com>
2019-11-15 15:26:30 +0800

    优化，去掉多余的代码

--- src/main/java/cn/ikangxu/boot/header/registrar/ImportHeaderRegistrar.java
+++ src/main/java/cn/ikangxu/boot/header/registrar/ImportHeaderRegistrar.java
@@ -13,19 +13,18 @@ import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
 
 /**
- *
- * @className ImportHeaderRegistrar
- * @description 
  * @author kangxu [xukang@engine3d.com]
- * @date 2019/10/18 11:14
  * @version v1.0
+ * @className ImportHeaderRegistrar
+ * @description
+ * @date 2019/10/18 11:14
  */
 public class ImportHeaderRegistrar implements ImportBeanDefinitionRegistrar {
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata metadata, BeanDefinitionRegistry registry) {
         AnnotationAttributes annotationAttributes = AnnotationAttributes.fromMap(metadata.getAnnotationAttributes(EnableHeader.class.getName(), false));
-        if(null != annotationAttributes) {
+        if (null != annotationAttributes) {
             String[] values = annotationAttributes.getStringArray("value");
 
             RootAttribute.bind(values);
